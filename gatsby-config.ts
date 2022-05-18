4bc54c5e0595a7fa68afdc6ed06ab7fc9a802155
websysarch <104688843+websysarch@users.noreply.github.com>
2022-05-18 22:05:33 +0530

    chore: initial commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,35 +1,47 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby'
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `websysarch-dot-com`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://websysarch.com`,
   },
-  plugins: ["gatsby-plugin-theme-ui", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "TODO"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    'gatsby-plugin-theme-ui',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'TODO',
+      },
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
     },
-    __key: "pages"
-  }]
-};
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/',
+      },
+      __key: 'pages',
+    },
+  ],
+}
 
-export default config;
+export default config
---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,10 @@
+const theme = {
+  fonts: {
+    body: '"Open Sans", sans-serif',
+    heading: '"Open Sans", sans-serif',
+    monospace: 'Menlo, monospace',
+  }
+}
+console.log("theme is loaded")
+
+export default theme
